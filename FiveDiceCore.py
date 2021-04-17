4142e567f28f9336288451cfca9de8831d4e773c
PipCheerio <82691736+PipCheerio@users.noreply.github.com>
2021-04-17 02:48:10 -0700

    Update FiveDiceCore.py

--- FiveDiceCore.py
+++ FiveDiceCore.py
@@ -67,81 +67,108 @@ class EvaluatePower:
     def pair(self):
         for x in range(0,6):
             if self.counted[x] == 2:
-                return True
+                return x+1
 
         return False
 
     def two_pair(self):
-        pair_count = 0
+        slice_list = []
         for x in range(0,6):
             if self.counted[x] == 2:
-                pair_count += 1
-        if pair_count == 2:
-            return True
+                slice_list.append(x)
+
+        if len(slice_list) == 2:
+            return (slice_list[1]+1)*10+(slice_list[0]+1)
 
         return False
 
     def three_of_a_kind(self):
         for x in range(0,6):
             if self.counted[x] == 3:
-                return True
+                return x+1
 
         return False
 
     def four_of_a_kind(self):
         for x in range(0,6):
             if self.counted[x] == 4:
-                return True
+                return x+1
 
         return False
 
 
     def five_of_a_kind(self):
-        for x in range(0,5):
+        for x in range(0,6):
             if self.counted[x] == 5:
-                return True
+                return x+1
 
         return False
 
     def full_house(self):
-        has_pair = False
-        has_trips = False
+        has_pair = 0
+        has_trips = 0
 
         for x in range(0,6):
             if self.counted[x] == 2:
-                has_pair = True
+                has_pair = x+1
             if self.counted[x] == 3:
-                has_trips = True
+                has_trips = x+1
         if has_pair and has_trips:
-            return True
+            return (has_trips*10)+has_pair
 
         return False
 
     def straight(self):
-        high_straight = [1,1,1,1,1,0]
-        low_straight = [0,1,1,1,1,1]
+        low_straight = [1,1,1,1,1,0]
+        high_straight = [0,1,1,1,1,1]
 
-        if self.hand == high_straight:
-            return True
-        if self.hand == low_straight:
-            return True
+        if self.counted == low_straight:
+            return 1
+
+        if self.counted == high_straight:
+            return 2
 
         return False
 
     def evaluate_power(self):
         if EvaluatePower.five_of_a_kind(self.counted):
-            return 7
+            return (7, EvaluatePower.five_of_a_kind(self.counted))
+
         elif EvaluatePower.four_of_a_kind(self.counted):
-            return 6
+            return (6, EvaluatePower.four_of_a_kind(self.counted))
+
         elif EvaluatePower.full_house(self.counted):
-            return 5
+            return (5, EvaluatePower.full_house(self.counted))
+
         elif EvaluatePower.straight(self.counted):
-            return 4
+            return (4, EvaluatePower.straight(self.counted))
+
         elif EvaluatePower.three_of_a_kind(self.counted):
-            return 3
+            return (3, EvaluatePower.three_of_a_kind(self.counted))
+
         elif EvaluatePower.two_pair(self.counted):
-            return 2
+            return (2, EvaluatePower.two_pair(self.counted))
+
         elif EvaluatePower.pair(self.counted):
-            return 1
+            return (1, EvaluatePower.pair(self.counted))
         else:
-            return 0
+            return (0, 0)
+
+class WhoWins:
+    # player is tuple (hand power) so is dealer
+    def __init__(self, player, dealer):
+        self.player = player
+        self.dealer = dealer
+        self.winner = self.player_wins()
+
+    def player_wins(self):
+        if self.player[0] == self.dealer[0]:
+            if self.dealer[1] >= self.player[1]:
+                return False
+            else:
+                return True
+
+        if self.dealer[0] >= self.player[1]:
+            return False
+        else:
+            return True
